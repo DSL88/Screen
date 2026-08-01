ccfd79862c5475dd3edab1fdff51161c2d42f640
Diogo <diogo@MacBook-Pro-de-Diogo.local>
2026-08-01 19:38:34 +0100

    96 - Adicionar menu Edit para restaurar colar/copiar (Cmd+V) no macOS
    - Alterados: main.js
    - Balance: 1 ficheiro(s), +14/-0

--- main.js
+++ main.js
@@ -409,6 +409,20 @@ function createWindow() {
       ]
     },
     {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' }
+      ]
+    },
+    {
       label: 'View',
       submenu: [
         { role: 'reload' },
